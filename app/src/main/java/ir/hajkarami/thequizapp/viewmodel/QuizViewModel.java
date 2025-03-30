f93c7b66bdffdae38ca413e1d832f0b8f5e6ba57
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-30 13:07:19 +0330

    Write ViewModel

--- app/src/main/java/ir/hajkarami/thequizapp/viewmodel/QuizViewModel.java
+++ app/src/main/java/ir/hajkarami/thequizapp/viewmodel/QuizViewModel.java
@@ -1,7 +1,24 @@
 package ir.hajkarami.thequizapp.viewmodel;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import ir.hajkarami.thequizapp.model.QuestionList;
+import ir.hajkarami.thequizapp.repository.QuizRepository;
+
 public class QuizViewModel extends ViewModel {
+    QuizRepository mQuizRepository = new QuizRepository();
+
+    LiveData<QuestionList> mQuestionListLiveData ;
+
+    public LiveData<QuestionList> getQuestionListLiveData() {
+        return mQuestionListLiveData;
+    }
+
+    public QuizViewModel() {
+        mQuestionListLiveData = mQuizRepository.getQuestionsFromAPI();
+    }
+
+
 
 }
