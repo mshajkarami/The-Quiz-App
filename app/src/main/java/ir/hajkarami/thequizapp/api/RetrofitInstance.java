7255b693da69289bb0031c0518f285d539487443
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-30 15:57:15 +0330

    Add network_security_config and add to manifest

--- app/src/main/java/ir/hajkarami/thequizapp/api/RetrofitInstance.java
+++ app/src/main/java/ir/hajkarami/thequizapp/api/RetrofitInstance.java
@@ -12,7 +12,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
 
-    String baseUrl = "http://127.0.0.1/quiz/";
+    String baseUrl = "http://10.0.2.2/quiz/";
 
     public Retrofit getRetrofitInstance() {
         return new Retrofit.Builder()
