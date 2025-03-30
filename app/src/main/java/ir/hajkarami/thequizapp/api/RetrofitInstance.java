26617ac4cd5221fc14e74fc234a6351fec1067b3
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-30 12:30:24 +0330

    Add Api interface

--- app/src/main/java/ir/hajkarami/thequizapp/api/RetrofitInstance.java
+++ app/src/main/java/ir/hajkarami/thequizapp/api/RetrofitInstance.java
@@ -1,4 +1,14 @@
 package ir.hajkarami.thequizapp.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitInstance {
+    String baseUrl = "http://127.0.0.1/quiz/";
+    public Retrofit getRetrofitInstance(){
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
 }
